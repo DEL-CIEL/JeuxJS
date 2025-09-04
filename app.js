b32608cf1c764a9b0b1a851c32f2f7c381f0ba60
Minimax Femto <charlemax10@gmail.com>
2025-09-04 09:38:51 +0200

    #2 création du serveur web

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('TP CIEL');
+var express = require('express');
+var port = 80;
+ 
+// Créer une instance de l’application express:
+var exp = express();
+// Paramétrer le répertoire racine du site avec la méthode use:
+exp.use(express.static(__dirname + '/www'));
+
+exp.get('/', function (req, res) {
+    console.log('Reponse a un client'); 
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+});
+
+exp.listen(port, function () {
+    console.log('Serveur en ecoute');
+});
