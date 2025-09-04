b774f0e0a90769fcbd9b4de392abf49481969a26
Minimax Femto <charlemax10@gmail.com>
2025-09-04 11:35:20 +0200

    #3 chat écrit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var express = require('express');
-var port = 80;
  
 // Créer une instance de l’application express:
 var exp = express();
@@ -18,6 +17,36 @@ exp.use(function (err, req, res, next) {
     res.status(500).send('Erreur serveur express');
 });
 
-exp.listen(port, function () {
+
+
+/*  *************** serveur WebSocket express *********************   */
+// 
+var expressWs = require('express-ws')(exp);
+
+// Connexion des clients à la WebSocket /echo et evenements associés 
+exp.ws('/echo', function (ws, req) {
+
+    console.log('Connection WebSocket %s sur le port %s',
+        req.connection.remoteAddress, req.connection.remotePort);
+
+    ws.on('message', function (message) {
+        console.log('De %s %s, message :%s', req.connection.remoteAddress,
+            req.connection.remotePort, message);
+        ws.send(message);
+    });
+
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion WebSocket %s sur le port %s',
+            req.connection.remoteAddress, req.connection.remotePort);
+    });
+
+});
+
+
+
+/*  ****** Serveur web et WebSocket en ecoute sur le port 80  ********   */
+//  
+var portServ = 80;
+exp.listen(portServ, function () {
     console.log('Serveur en ecoute');
 });
